69bb9f59214787c10ed71a5cf907cd1665d61934
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2018-05-02 17:06:53 +0200

    Adding 'health' to cluster's verbs.

--- eslib/cluster/__init__.py
+++ eslib/cluster/__init__.py
@@ -1,8 +1,9 @@
 from asyncio import coroutine
 
 from eslib.dispatcher import dispatcher, command, Dispatcher
-from eslib.verb import ReadSettings, WriteSettings
+from eslib.verb import ReadSettings, WriteSettings, DumpVerb
 
+import json
 
 @dispatcher(object_name="cluster")
 class ClusterDispatcher(Dispatcher):
@@ -10,6 +11,43 @@ class ClusterDispatcher(Dispatcher):
     @coroutine
     def check_noun_args(self, running):
         pass
+
+
+@command(ClusterDispatcher, verb='health')
+class ClusterHealth(DumpVerb):
+
+    @coroutine
+    def get(self, running):
+        return None
+
+    @coroutine
+    def check_verb_args(self, running, *args, **kwargs):
+        running.args = args
+        if len(args) > 0:
+            flat = True
+        yield from super().check_verb_args(running, *args, **kwargs)
+
+    @coroutine
+    def execute(self, running):
+        val = yield from self.api.escnx.cluster.health(level='cluster')
+        return val
+
+    def to_str(self, running, item):
+        if item is None:
+            return None
+        if len(running.args) == 1:
+            if running.args[0] in item:
+               return item[running.args[0]]
+        if len(running.args) > 0:
+            values = {}
+            for i in running.args:
+                if i in item:
+                    values[i] = item[i]
+            return json.dumps(values, **running.formatting)
+        elif running.only_keys:
+            return json.dumps(list(item.keys()), **running.formatting)
+        else:
+            return json.dumps(item, **running.formatting)
 
 
 @command(ClusterDispatcher, verb='readsettings')
