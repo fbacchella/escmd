d887556d98350edda7bf9ba02663690d686d5171
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2018-05-03 10:55:06 +0200

    Improving nodes verbs

--- eslib/nodes/__init__.py
+++ eslib/nodes/__init__.py
@@ -16,22 +16,41 @@ class NodesDispatcher(Dispatcher):
 
     @coroutine
     def get(self, running):
-        nodes = yield from self.api.escnx.nodes.info(running.node_name)
+        if running.node_name is None or len(running.node_name) == 0:
+            node_name = '*'
+        else:
+            node_name = running.node_name
+        nodes = yield from self.api.escnx.nodes.info(node_name, metric=running.metrics)
         return nodes['nodes']
 
 
 @command(NodesDispatcher)
 class NodesList(List):
 
+    @coroutine
+    def check_verb_args(self, running, *args, **kwargs):
+        running.metrics = 'host'
+        yield from super().check_verb_args(running, *args, **kwargs)
+
     def to_str(self, running, item):
-        name = item[0]
+
         value = item[1]
-        return "%s %s '%s'" % (name, value['host'], ', '.join(value['roles']))
+        value.pop('attributes')
+        value.pop('build_hash')
+        return "%s %s" % (value['name'], value)
 
 
 @command(NodesDispatcher, verb='dump')
 class NodesDump(DumpVerb):
-    pass
+
+    def fill_parser(self, parser):
+        super().fill_parser(parser)
+        parser.add_option("-m", "--metric", dest="metrics", default=[], action='append')
+
+    @coroutine
+    def check_verb_args(self, running, *args, metrics=[], **kwargs):
+        running.metrics = ','.join(metrics)
+        yield from super().check_verb_args(running, *args, **kwargs)
 
 
 @command(NodesDispatcher)
