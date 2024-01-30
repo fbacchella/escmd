e3f9a7170e9eb43d5d84b3c473282c814cc5904d
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2024-01-30 17:19:22 +0100

    Improvement to searchguard verbs

--- eslib/searchguard/__init__.py
+++ eslib/searchguard/__init__.py
@@ -22,7 +22,7 @@ class SearchGuardDispatcher(Dispatcher):
         return await self.api.escnx.transport.perform_request(method, '/_searchguard' + url, headers=headers, **kwargs)
 
 
-@command(SearchGuardDispatcher, verb='whomai')
+@command(SearchGuardDispatcher, verb='whoami')
 class Authinfo(DumpVerb):
 
     async def get(self, running, **kwargs):
@@ -35,18 +35,33 @@ class Authinfo(DumpVerb):
         return super().to_str(running, (None, (None, item)))
 
 
+@command(SearchGuardDispatcher, verb='debug')
+class Debug(DumpVerb):
+
+    async def get(self, running, **kwargs):
+        return {}
+
+    async def execute(self, running, **kwargs):
+        return await self.dispatcher.search_guard_query(running, 'GET', '/auth/debug')
+
+    def to_str(self, running, item):
+        return super().to_str(running, (None, (None, item)))
+
+
 @command(SearchGuardDispatcher, verb='authtoken')
 class AuthToken(DumpVerb):
 
     def fill_parser(self, parser):
         parser.add_option("-n", "--name", dest="name", help="Token name", default=None)
         parser.add_option("-e", "--expires", dest="expires", help="Expires after", default=None)
+        parser.add_option("-f", "--freeze_privileges", dest="freeze_privileges", help="Freeze privileges", default=None, action='store_true')
 
-    def check_verb_args(self, running, *args, name=None, expires=None, **kwargs):
+    def check_verb_args(self, running, *args, name=None, expires=None, freeze_privileges=None, **kwargs):
         if name is None:
             raise Exception('Name is mandatory')
         else:
             running.name = name
+        running.freeze_privileges = freeze_privileges
         running.expires = expires
         running.requested = {
                 "index_permissions":
@@ -70,7 +85,13 @@ class AuthToken(DumpVerb):
         }
         if running.expires is not None:
             body['expires_after'] = running.expires
+        if running.freeze_privileges is not None:
+            body['freeze_privileges'] = running.freeze_privileges
         return await self.dispatcher.search_guard_query(running, 'POST', '/authtoken', body=body)
 
     def to_str(self, running, item):
-        return super().to_str(running, (None, (None, item)))
+        if 'token' in item:
+            return item['token']
+        else:
+            return super().to_str(running, (None, (None, item)))
+
