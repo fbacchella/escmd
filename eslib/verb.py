5e122bceebe6bfda53f3c834f6a74ac9fcae249e
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2026-08-02 11:40:46 +0200

    Another API breaking change in python, with collections.abc.Mapping

--- eslib/verb.py
+++ eslib/verb.py
@@ -379,7 +379,7 @@ class WriteSettings(RepeterVerb):
         """
         for k, v in merge_dct.items():
             if (k in dct and isinstance(dct[k], dict)
-                    and isinstance(merge_dct[k], collections.Mapping)):
+                    and isinstance(merge_dct[k], collections.abc.Mapping)):
                 self._dict_merge(dct[k], merge_dct[k])
             else:
                 dct[k] = merge_dct[k]
